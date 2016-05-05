eebbf83547086a108628e8415f25201743aa018e
halbg <guillaume.halb@free.fr>
2016-05-05 11:29:57 +0200

    Ajout de pann en param

--- Bouton/Test.java
+++ Bouton/Test.java
@@ -5,7 +5,8 @@ import java.awt.Color;
 public class Test {
 
     public static void main(String[] args) {
+        Panneau pann = new Panneau();
         Menu m = new Menu();
-        Fenetre1 fen = new Fenetre1();
+        Fenetre1 fen = new Fenetre1(pann);
     }
 }
